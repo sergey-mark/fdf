9c0d79b4c03208a6e0d4489839cbc0b4a372b801
pbillett <pbillett@student.42.fr>
2016-08-04 12:06:59 +0200

    Drawline finally works. Affectation of data_adress to new img ok. Work on expose when masking or hiding window, Escape to quit window, numpad arrow to pan left/up/right/down, Mode point or wireframe and filled(need to work on it) with 123num classique. Plus/minus to zoom, and pageup/pagedown to increase level of the fields. Still pb on color during pixel address affectation. Still working also on rotation of the figure. (inverse side of reading only work for turn in 90/180/270). And use of expression of matrix rotation X or Y only works on one side (and not good). Need to work on more complete solution with euler angle with all side combined matrice rotation (xyz). Ideally with mouse. Need to work of height of color to manage bonus. And change side of endian to don't hide some faces. (need to start from top right) (not bottom left). Need to do an other read in iso to change projection type. (from para to iso). And manage few colors as parameters

--- main.c
+++ main.c
@@ -2,8 +2,7 @@
 #include "fdf.h"
 #include <math.h>
 #include <stdio.h>
-
-#define ACCENTUATION 10 // On peut mettre une accentuation à 10 pour compenser les maps avec des petits chiffres ;)
+#include <stdlib.h>
 
 /*
 int		my_mouse_function(int keycode, t_wind *w)
@@ -28,25 +27,84 @@ int		my_mouse_function(int keycode, t_wind *w)
 	return (0);
 }*/
 
-int		my_key_function(int keycode, t_wind *w)
+double		degree_to_radian(int degrees)
 {
-	ft_putendl("Keyevent");
-	ft_putnbr(keycode);
+	double	radians;
+	double	pi;
+
+	pi = 3.1415;
+	radians = degrees*(pi/180);
+	return (radians);
+}
+
+t_point		rotation_point(t_point A, int angle)
+{
+	t_point B;
+	double	r_angle;
+	double	my_cos;
+	double	my_sin;
+	//double	result;
+	//double	x;
+	
+	ft_putstr("Ax:");
+	ft_putnbr(A.x);
+	ft_putstr("Ay:");
+	ft_putnbr(A.y);
+	ft_putstr("Az:");
+	ft_putnbr(A.z);
+	ft_putendl("end");
+/*
+	x = 1.0471;
+	result = cos(x);
+	printf("cos(%.4lf) = %.2lf\n", x, result);
+*/
+
+	r_angle = degree_to_radian(angle);
+	ft_putstr("angle:");
+	ft_putnbr(angle);
 	ft_putchar('\n');
-	if (keycode == 65361 && w->point.x >= 0) // fleche gauche
-		w->point.x -= 5;
-	else if (keycode == 65363 && w->point.x < w->width) // fleche droite
-		w->point.x += 5;
-	else if (keycode == 65362 && w->point.y >= 0) // fleche haut
-		w->point.y -= 5;
-	else if (keycode == 65364 && w->point.y < w->height) // fleche bas
-		w->point.y += 5;
-	mlx_pixel_put(w->mlx, w->win, w->point.x, w->point.y, w->point.color);
-	ft_putnbr(w->point.y);
-	ft_putchar('\n');
-	ft_putnbr(w->point.x);
-	ft_putchar('\n');
-	return (0);
+	ft_putstr("angle radians:");
+	printf("r_angle = %.4lf\n", r_angle);
+	my_cos = cos(r_angle);
+	my_sin = sin(r_angle);
+	printf("cos(%.4lf) = %.2lf\n", r_angle, my_sin);
+	printf("sin(%.4lf) = %.2lf\n", r_angle, my_cos);
+	
+	printf("A.x * cos(r_angle) = %.4lf\n", A.x * cos(r_angle));
+	printf("A.y * sin(r_angle) = %.4lf\n", A.y * sin(r_angle));
+	/*
+	ft_putnbr(my_cos);
+	ft_putstr("-");
+	ft_putnbr(my_sin);
+	ft_putstr("!");
+	*/
+	//B.x = ((A.x - i->x_centerpoint) * cos(r_angle)) - ((A.y - i->y_centerpoint)  * sin(r_angle)); // include <math.h>
+	//B.y = ((A.x - i->x_centerpoint) * sin(r_angle)) + ((A.y - i->y_centerpoint)  * cos(r_angle));
+	//B.z = A.z;
+	
+	// Rotation axe x:
+	//B.x = A.x;
+	//B.y = (A.y * cos(r_angle)) - (A.z * sin(r_angle)); // include <math.h>
+	//B.z = (A.y * sin(r_angle)) + (A.z * cos(r_angle));
+	
+	// Rotation axe y:
+	//B.x = (A.x * cos(r_angle)) + (A.z * sin(r_angle)); // include <math.h>
+	//B.y = A.y;
+	//B.z = -(A.x * sin(r_angle)) + (A.z * cos(r_angle));
+	
+	// Rotation axe z:
+	B.x = (A.x * cos(r_angle)) - (A.y * sin(r_angle)); // include <math.h>
+	B.y = (A.x * sin(r_angle)) + (A.y * cos(r_angle));
+	B.z = A.z;
+	
+	ft_putstr("Bx:");
+	ft_putnbr(B.x);
+	ft_putstr("By:");
+	ft_putnbr(B.y);
+	ft_putstr("Bz:");
+	ft_putnbr(B.z);
+	ft_putendl("end");
+	return (B);
 }
 
 int			get_sign(int first, int second)
@@ -96,13 +154,18 @@ void		color_point(int num, size_t *color)
 
 void		color_line(int num1, int num2, size_t *color)
 {
+	size_t white;
+	size_t purple;
+
+	white = 0x00FFFFFF;
+	purple = 0x00FF00FF;
 	if (num1 == 10 && num2 == 10)
-		*color = 0x00FF00FF;
+		*color = purple;
 	else
-		*color = 0x00FFFFFF;
+		*color = white;
 }
 
-int			draw_line(t_wind **w, int z, int xdest, int ydest)
+int			draw_line(int xdest, int ydest, t_img **i)
 { 
 	float	x;
 	float	y;
@@ -112,13 +175,13 @@ int			draw_line(t_wind **w, int z, int xdest, int ydest)
 	float		diff_x;
 	float		diff_y;
 
-	x = (*w)->point.x;
-	y = (*w)->point.y - z;
+	x = (*i)->r_point.x;
+	y = (*i)->r_point.y - (*i)->r_point.z;
 	sign_x = get_sign(x, xdest);
 	sign_y = get_sign(y, ydest);
 	diff_x = get_diff(x, xdest);
 	diff_y = get_diff(y, ydest);
-
+	/*
 	ft_putstr("x : ");
 	ft_putnbr(x);
 	ft_putchar('\n');
@@ -143,53 +206,50 @@ int			draw_line(t_wind **w, int z, int xdest, int ydest)
 	ft_putstr("sign_y : ");
 	ft_putnbr(sign_y);
 	ft_putchar('\n');
-
-		if (diff_x > diff_y)
-			bigdiff = diff_x;
-		else if (diff_y > diff_x)
-			bigdiff = diff_y;
-		else
-			bigdiff = diff_y;
+	*/
+	if (diff_x > diff_y)
+		bigdiff = diff_x;
+	else if (diff_y > diff_x)
+		bigdiff = diff_y;
+	else
+		bigdiff = diff_y;
+	/*
 	ft_putstr("bigdiff : ");
-	ft_putnbr(bigdiff);
-	ft_putchar('\n');
+	ft_putnbr(bigdiff);*/
 	while (rint(x) != xdest || rint(y) != ydest)
 	{
 		if (x != xdest)
 		{
 			x = x + (sign_x * (diff_x/bigdiff));
-			printf("%.2f ", x);
-			//ft_putnbr(x);
-			//ft_putchar(' ');
+			//printf("%.2f ", x);
 		}
 		if (y != ydest)
 		{
 			y = y + (sign_y * (diff_y/bigdiff));
-			//printf("%.2f", (float)((float)diff_y/(float)bigdiff));
-			printf("%.2f ", y);
-			//ft_putnbr(y);
-			//ft_putchar(' ');
+			//printf("%.2f ", y);
 		}
-		//ft_putstr("ma fin");
-		//break ;
-		//ft_putnbr(x);
-		//ft_putchar('-');
-		//ft_putnbr(y);
-		//ft_putchar(' ');
-		mlx_pixel_put((*w)->mlx, (*w)->win, rint(x), rint(y), (*w)->point.color);
+	/*ft_putchar(':');
+	ft_putnbr(rint(x));
+	ft_putchar(':');
+	ft_putnbr(rint(y));
+	ft_putchar(':');
+	*/
+		//mlx_pixel_put((*w)->mlx, (*w)->win, rint(x), rint(y), (*w)->point.color);
+		//ft_putstr("tata");
+		*((*i)->pxl_ptr+(int)((rint(y))*((*i)->size_line)+ (rint(x))*((*i)->octet_per_pixel))) = (int)"0x00FFFFFF";
+
+		//*((*i)->pxl_ptr+(int)((rint(y)*(w->p.size_square-(z)))*(((*i)->size_line)) + (rint(x))*((*i)->octet_per_pixel))) = (int)"#00FFFFFF";
 	}
 	return (0);
 }
 
-int			**browsefile(char *filename, int *nbr_of_line, int *nbr_elem_line)
+int			**browsefile(char *filename, int *y, int *x)
 {
 	int		fd;
 	int		fd1;
 	char	*line;
 	char	**tab;
 	int		**tab_int;
-	int		y;
-	int		x;
 
 	//fd = open("./test_maps/42.fdf", O_RDONLY);
 	fd = open(filename, O_RDONLY);
@@ -198,80 +258,77 @@ int			**browsefile(char *filename, int *nbr_of_line, int *nbr_elem_line)
 		return (NULL);
 	tab = NULL;
 	tab_int = NULL;
-	y = 0;
+	(*y) = 0;
 	while (get_next_line(fd1, &line))
-		y++;
-	tab_int = malloc((y+1) * sizeof(int *));
+		(*y)++;
+	tab_int = malloc(((*y)+1) * sizeof(int *));
 	
-	y = 0;
+	(*y) = 0;
 	while (get_next_line(fd, &line))
 	{
 		ft_putendl(line);
-		tab_int[y] = malloc(ft_strlen(line) * sizeof(int));
+		tab_int[(*y)] = malloc(ft_strlen(line) * sizeof(int));
 		tab = ft_strsplit(line, ' ');//nbr d'elements par lignes
-		x = 0;
-		while (tab[x])
+		(*x) = 0;
+		while (tab[(*x)])
 		{
-			tab_int[y][x] = ft_atoi(tab[x]);
-			x++;
+			tab_int[(*y)][(*x)] = ft_atoi(tab[(*x)]);
+			(*x)++;
 		}
-		y++;
+		(*y)++;
 	}
-	*nbr_of_line = y;
-	*nbr_elem_line = x;
 	return (tab_int);
 }
 
-int			checkpoint_allside(t_wind *w, int x, int y, int nbr_elem_line, int nbr_of_line, int **tab_int)
+int			checkpoint_allside(int x, int y, t_browsefile b, t_img *i, t_wind *w)
 {
-	int		z;
 	int		z_left;
 	int		z_right;
 	int		z_up;
 	int		z_down;
 
-	z = tab_int[y][x];
-	z = z*ACCENTUATION; // Ajout du parametre d'accentuationau point d'origine.
+	//z = b.tab_int[y][x];
+	//z = z*w->p.accentuation; // Ajout du parametre d'accentuationau point d'origine.
 	if(x>0)//Si point à gauche
 	{
 		//ft_putendl("point à gauche");
-		z_left = (tab_int[y][x-1]);
-		color_line(z, z_left, &w->point.color);
+		z_left = (b.tab_int[y][x-1]);
+		color_line(i->r_point.z, z_left, &i->point.color);
 		//ft_putnbr(z_left);
 		//ft_putchar('\n');
-		draw_line(&w, z, (w->point.x - 30), (w->point.y - (z_left*ACCENTUATION)));
+		draw_line((i->r_point.x - w->p.size_square), i->r_point.y - (z_left*w->p.accentuation), &i);
 	}
-	if(x<(nbr_elem_line-1))//Si point à droite
+	if(x<(b.nbr_elem_line-1))//Si point à droite
 	{
 		//ft_putendl("point à droite");
-		z_right = (tab_int[y][x+1]);
-		color_line(z, z_right, &w->point.color);
+		z_right = (b.tab_int[y][x+1]);
+		color_line(i->r_point.z, z_right, &i->point.color);
 		//ft_putnbr(z_right);
 		//ft_putchar('\n');
-		draw_line(&w, z, w->point.x + 30, w->point.y - (z_right*ACCENTUATION));
+		draw_line(i->r_point.x + w->p.size_square, i->r_point.y - (z_right*w->p.accentuation), &i);
 	}
 	if(y>0)//Si point au dessus
 	{
 		//ft_putendl("point au dessus");
-		z_up = (tab_int[y-1][x]);
-		color_line(z, z_up, &w->point.color);
+		z_up = (b.tab_int[y-1][x]);
+		color_line(i->r_point.z, z_up, &i->point.color);
 		//ft_putnbr(z_up);
 		//ft_putchar('\n');
-		draw_line(&w, z, (w->point.x + 30), (w->point.y - 30 - (z_up*ACCENTUATION)));
+		draw_line((i->r_point.x + w->p.size_square), (i->r_point.y - w->p.angle_projpara - (z_up*w->p.accentuation)), &i);
 	}
-	if(y<(nbr_of_line-1))//Si point en dessous
+	if(y<(b.nbr_of_line-1))//Si point en dessous
 	{
 		//ft_putendl("point au dessous");
-		z_down = (tab_int[y+1][x]);
-		color_line(z, z_down, &w->point.color);
+		z_down = (b.tab_int[y+1][x]);
+		color_line(i->r_point.z, z_down, &i->point.color);
 		//ft_putnbr(z_down);
 		//ft_putchar('\n');
-		draw_line(&w, z, w->point.x - 30, w->point.y + 30 - (z_down*ACCENTUATION));
+		draw_line(i->r_point.x - w->p.size_square, i->r_point.y + w->p.angle_projpara - (z_down*w->p.accentuation), &i);
 	}
 	return (1);
 }
 //int			fill_3d_map(char *pxl_ptr, int bits_per_pixel, int size_line, int x, int y, int endian_type, int type)
-int			fill_3d_map(int **tab_int, int nbr_of_line, int nbr_elem_line, t_wind *w)
+int			fill_3d_map(t_browsefile b, t_img *i, t_wind *w)
 {
 	int		y;
 	int		x;
@@ -279,57 +336,204 @@ int			fill_3d_map(int **tab_int, int nbr_of_line, int nbr_elem_line, t_wind *w)
 	int		start_x;
 
 	ft_putendl("ok");
-	start_x = w->point.x;
-	y = nbr_of_line;
+	i->octet_per_pixel = i->bits_per_pixel/8;
+	ft_putstr("octet par pixel");
+	ft_putnbr(i->octet_per_pixel);
+	ft_putchar('\n');
+
+	//*(i->pxl_ptr) = (int)"0xFFFFFF";
+	//x = 0;
+	//while (x < 250)
+		//*(i->pxl_ptr+(x++)*(i->bits_per_pixel)) = (size_t)"0x00FFFFFF";
+	//*i->pxl_ptr = (int)"0x00FFFFFF";
+	i->point.x = 50;
+	start_x = i->point.x;
+	i->point.y = 550;
+	y = b.nbr_of_line;
 	y--;
 	while (y >= 0)
 	{
-		//ft_putlstnbr(tab_int[y], x);
-		w->point.y -= 30; // On ecarte de 25 pixels les chiffres et les lignes pour mieux voir les points ;)
+		i->point.y -= w->p.angle_projpara; // On ecarte de 25 pixels les chiffres et les lignes pour mieux voir les points ;)
+		//*(i->pxl_ptr) = *(int*)"0x00FFFFFF";
+		//*(i->pxl_ptr+y*(i->size_line)) = (size_t)"0x00FFFFFF";
 		// Puis on décrémente pour partir du bas à gauche de l'écran pour commencer à afficher les points. (en little endian)
-		start_x += 30;
-		w->point.x = start_x; //Pour créer le décalage à chaque début de ligne on met x à 25 de plus au lieu de faire partir à zéro.
+		start_x += w->p.size_square;
+		i->point.x = start_x; //Pour créer le décalage à chaque début de ligne on met x à 25 de plus au lieu de faire partir à zéro.
 		x = 0;
-		while (x < nbr_elem_line)
+		while (x < b.nbr_elem_line)
 		{
-			z = tab_int[y][x];
+			z = b.tab_int[y][x];
+			//i->point.z = b.tab_int[y][x];
+			i->point.z = b.tab_int[y][x] * w->p.accentuation;
 			ft_putnbr(z);
 			ft_putchar('-');
-			color_point(z, &w->point.color);//Je colore mon point si different de zero
-			mlx_pixel_put(w->mlx, w->win, w->point.x, (w->point.y - (z*ACCENTUATION)), w->point.color);
-			checkpoint_allside(w, x, y, nbr_elem_line, nbr_of_line, tab_int);
-			w->point.x += 30;
+			//color_point(z, &i->point.color);//Je colore mon point si different de zero
+			//*(i->pxl_ptr+((y*w->p.size_square-(z))*(i->size_line)) + (i->point.x)*(i->bits_per_pixel)) = (int)"0x00FFFFFF";
+			if (x == b.nbr_elem_line/2 && y == b.nbr_of_line/2)
+			{
+				i->x_centerpoint = i->point.x;
+				i->y_centerpoint = i->point.y;
+				ft_putstr("center point:");
+				ft_putnbr(i->point.x);
+				ft_putstr(":");
+				ft_putnbr(i->point.y);
+				ft_putchar('\n');
+			}
+			/*
+				ft_putstr("x:");
+				ft_putnbr(i->point.x);
+				ft_putstr("y:");
+				ft_putnbr(i->point.y);
+				ft_putstr("z:");
+				ft_putnbr(i->point.z);
+				ft_putchar('\n');
+			*/
+				// On applique la rotation si on appui sur la flèche de haut ou bas
+				i->r_point = rotation_point(i->point, w->p.rot_x);
+			/*
+				ft_putstr("rotx:");
+				ft_putnbr(i->r_point.x);
+				ft_putstr("roty:");
+				ft_putnbr(i->r_point.y);
+				ft_putstr("rotz:");
+				ft_putnbr(i->r_point.z);
+				ft_putchar('\n');
+			*/
+
+			if (w->p.graphic_mode == 1) // Mode point
+				*((i->pxl_ptr)+((i->r_point.y - i->r_point.z)*(int)(i->size_line))+((i->r_point.x)*(i->octet_per_pixel))) = (int)"0x00FFFFFF";
+			else if (w->p.graphic_mode == 2) // Mode filaire
+				checkpoint_allside(x, y, b, i, w);
+			else if (w->p.graphic_mode == 3) // Mode rempli
+				checkpoint_allside(x, y, b, i, w);
+			i->point.x += w->p.size_square;
 			x++;
 		}
 		ft_putendl("|");
 		y--;
 	}
-	//draw_line(&w, z, (w->point.x - 30), (w->point.y - z + 70));
 	return (0);
 }
 
-int			main(int argc, char **argv)
+int		my_expose_hook(t_wind *w)
 {
-	t_wind	w;
-	void	*img_ptr;
-	int		bits_per_pixel;
-	int		size_line;
-	int		endian_type;
-	int		**tab_int;
-	int		nbr_of_line;
-	int		nbr_elem_line;
-	//char	*pxl_ptr;
-	/*
-	float	my_nb;
-	int		ty;
+	mlx_put_image_to_window(w->mlx, w->win, w->img.ptr_img, w->img.x, w->img.y);
+	return (0);
+}
 
-	my_nb = 3.1416;
-	ty = round(my_nb);
-	ft_putnbr(ty);
-	ft_putstr("tata");
-	printf("floatx: %.2f", 3.1416);
-	ft_putstr("tata");
-	*/
+int		create_new_img(t_wind *w)
+{
+	w->img.width = 1024;
+	w->img.height = 600;
+
+	w->img.ptr_img = mlx_new_image(w->mlx, w->img.width, w->img.height);
+	w->img.pxl_ptr = mlx_get_data_addr(w->img.ptr_img, &w->img.bits_per_pixel, &w->img.size_line, &w->img.endian_type);
+	printf("bits_per_pixel: %d\nsize_line: %d\nendian_type: %d\n", w->img.bits_per_pixel, w->img.size_line, w->img.endian_type);
+
+	fill_3d_map(w->b, &w->img, w);
+	return (0);
+}
+
+int		my_key_function(int keycode, t_wind *w)
+{
+	//int	x_oldcenterpoint;
+	//int	y_oldcenterpoint;
+
+	ft_putchar('\n');
+	ft_putendl("Keyevent");
+	ft_putnbr(keycode);
+	ft_putchar('\n');
+
+	// ECHAP (to quit)
+	if (keycode == 65307)
+		exit(0);
+
+	// ROTATIONS:
+	if (keycode == 65361 && w->img.x >= 0) // fleche gauche
+		w->img.x -= 5;
+	else if (keycode == 65363 && w->img.x < w->width) // fleche droite
+		w->img.x += 5;
+	else if (keycode == 65362 && w->img.y >= 0) // fleche haut
+		w->p.rot_x += 1;
+	else if (keycode == 65364 && w->img.y < w->height) // fleche bas
+		w->p.rot_x -= 1;
+
+	// DEPLACEMENT LATERAL (pan) (Pavé numérique)
+	if (keycode == 65431)//haut (pav num)
+		w->img.y -= 5;
+	else if (keycode == 65433)//bas (pav num)
+		w->img.y += 5;
+	else if (keycode == 65432)//droite (pav num)
+		w->img.x += 5;
+	else if (keycode == 65430)//gauche (pav num)
+		w->img.x -= 5;
+
+	// ZOOM:
+	if (keycode == 65451)//plus (pav num)
+	{
+		//w->p.accentuation++;
+		w->p.size_square++; 
+		w->p.angle_projpara++;
+		//x_oldcenterpoint = w->img.x_centerpoint;
+		//y_oldcenterpoint = w->img.y_centerpoint;
+		//w->img.x += w->img.x_centerpoint - x_oldcenterpoint; // Recentrage du zoom au milieu
+		//w->img.y += w->img.y_centerpoint - y_oldcenterpoint; // Recentrage du zoom au milieu
+	}
+	if (keycode == 65453)//moins (pav num)
+	{
+		//w->p.accentuation--;
+		w->p.size_square--; 
+		w->p.angle_projpara--;
+		//w->img.x = w->img.x_centerpoint - (w->img.width/2); // Recentrage du zoom au milieu
+		//w->img.y = w->img.y_centerpoint - (w->img.height/2); // Recentrage du zoom au milieu
+	}
+
+	// ACCENTUATION (Hauteur du terrain)
+	if (keycode == 65365)//page up
+	{
+		w->p.accentuation++;
+		//w->p.size_square++; 
+		//w->p.angle_projpara++;
+	}
+	if (keycode == 65366)//page down
+	{
+		w->p.accentuation--;
+		//w->p.size_square--; 
+		//w->p.angle_projpara--;
+	}
+
+	// GRAPHIC MODES: (touche 1 à zéro)
+	if (keycode == 38)//1 poitille
+		w->p.graphic_mode = 1;
+	else if (keycode == 233)//2 filaire (par default)
+		w->p.graphic_mode = 2;
+	else if (keycode == 34)//2 rempli (par default)
+		w->p.graphic_mode = 3;
+
+	//F2 Vue Parallele
+	//F3 Vue Isometrique
+
+	//Gestion des couleurs
+
+	//1 solide de base
+	//2 smooth+solide de base
+	//3 smooth
+	//4 filaire
+	//mlx_pixel_put(w->mlx, w->win, w->point.x, w->point.y, w->point.color);
+	//ft_putnbr(w->point.y);
+	//ft_putchar('\n');
+	//ft_putnbr(w->point.x);
+	//ft_putchar('\n');
+	mlx_destroy_image(w->mlx, w->img.ptr_img);
+	create_new_img(w);
+	mlx_put_image_to_window(w->mlx, w->win, w->img.ptr_img, w->img.x, w->img.y);
+	return (0);
+}
+
+int					main(int argc, char **argv)
+{
+	t_wind			w;
+
 	if (argc != 2)
 		ft_putstr("usage");
 	ft_putendl("fdf");
@@ -338,17 +542,40 @@ int			main(int argc, char **argv)
 	w.height = 800;
 	w.title = "42 minilibx";
 	w.win = mlx_new_window(w.mlx, w.width, w.height, w.title);
-	w.point.x = 50;
-	w.point.y = 500;
-	w.point.color = 0x00FFFFFF;
 
-	img_ptr = mlx_new_image(w.mlx, 100, 100);
-	/*pxl_ptr = */mlx_get_data_addr(&img_ptr, &bits_per_pixel, &size_line, &endian_type);
-	//fill_3d_map(pxl_ptr, bits_per_pixel, size_line, 0, 0, endian_type, 1);
-	tab_int = browsefile(argv[1], &nbr_of_line, &nbr_elem_line);
-	fill_3d_map(tab_int, nbr_of_line, nbr_elem_line, &w);
-	mlx_put_image_to_window(w.mlx, w.win, img_ptr, 100, 100);
+	ft_putstr("degree_to_radian :");
+	ft_putnbr(degree_to_radian(90));
+	ft_putchar('\n');
+
+	
+
+	w.img.x_centerpoint = 500;
+	w.img.y_centerpoint = 370;
+
+	// Parameters:
+	w.p.graphic_mode = 2; // Mode filaire par défault (touche nombre pour changer)
+	w.p.accentuation = 4;// On peut mettre une accentuation à 10 pour compenser les maps avec des petits chiffres ;). Default = 1
+	w.p.angle_projpara = 30;
+	w.p.size_square = 30;
+	w.p.rot_x = 0; // Rotation x par défault
+
+	//w.img.point.x = 50;
+	//w.img.point.y = 500;
+	w.img.point.color = 0xFFFFFF;
+
+	(w.b.nbr_of_line) = 0;
+	(w.b.nbr_elem_line) = 0;
+	w.b.tab_int = browsefile(argv[1], &w.b.nbr_of_line, &w.b.nbr_elem_line);
+
+	create_new_img(&w);
+	//Position of image in window
+	w.img.x = 0;
+	w.img.y = 0;
+	mlx_put_image_to_window(w.mlx, w.win, w.img.ptr_img, w.img.x, w.img.y);
+	//*(w.img.pxl_ptr + 32) = (int)"0x00FFFFFF";
+
 	mlx_key_hook(w.win, my_key_function, &w);
+	mlx_expose_hook(w.win, my_expose_hook, &w);
 	//mlx_mouse_hook(w.win, my_mouse_function, &w);
 	mlx_loop(w.mlx);
 	return (0);
--- struct.h
+++ struct.h
@@ -2,22 +2,62 @@
 # ifndef STRUCT_H
 # define STRUCT_H
 
-typedef struct	s_point
+typedef struct		s_point
 {
-	int			x;
-	int			y;
-	size_t		color;
-}				t_point;
+	int				x;
+	int				y;
+	int				z;
+	size_t			color;
+}					t_point;
 
-typedef struct	s_wind
+typedef struct		s_browsefile
 {
-	void		*mlx;
-	void		*win;
-	char		*title;
-	int			width;
-	int			height;
-	t_point		point;
-}				t_wind;
+	int				**tab_int;
+	int				nbr_of_line;
+	int				nbr_elem_line;
+}					t_browsefile;
+
+typedef struct		s_img
+{
+	void			*ptr_img;
+	int				x;
+	int				y;
+	int				width;
+	int				height;
+	int				x_centerpoint;
+	int				y_centerpoint;
+	char			*pxl_ptr;
+	int				bits_per_pixel;
+	int				size_line;
+	int				octet_per_pixel;
+	int				endian_type;
+	t_point			point; //avant calcul rotation
+	t_point			r_point; //apres caclul rotation
+}					t_img;
+
+typedef struct		s_params
+{
+	int				accentuation; //hauteur relief
+	int				angle_projpara; //zoom y
+	int				size_square; //zoom x
+	int				graphic_mode; // Mode
+	int				rot_x;
+	int				rot_y;
+	int				rot_z;
+}					t_params;
+
+typedef struct		s_wind
+{
+	void			*mlx;
+	void			*win;
+	char			*title;
+	int				width;
+	int				height;
+	t_point			point;
+	t_img			img;
+	t_browsefile	b;
+	t_params		p;
+}					t_wind;
 
 #endif
 
